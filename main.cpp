8754973a69830a2c131c241ccd619824a1727ca3
dogpie <dankov.ns@phystech.edu>
2022-03-20 19:31:07 +0300

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "vector"
+#include "SFML/Graphics.hpp"
+
+#include "sources/entity.h"
+#include "sources/game.h"
+#include "sources/dynamic_graphics.h"
+#include "sources/player.h"
+
+
+int main() {
+  GameLoop game_loop;
+  game_loop.Run();
+}
